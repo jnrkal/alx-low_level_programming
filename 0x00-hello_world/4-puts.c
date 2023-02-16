be35ba9ce14b79067cfffc45f5da09750a0d8886
jnrkal <junioreric35@gmail>
2023-02-16 15:51:54 -0800

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* A C program that prints by using the puts function
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like buildong a multilingual puzzle");
+	return (0);
+}	
