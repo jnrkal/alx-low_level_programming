2e4d93a09c04a32aeab54352137f3f8ff45465a8
jnrkal <junioreric35@gmail>
2023-02-16 18:28:08 -0800

    Size not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+
+/**
+ * main - A program that prints the size of various types on the computer;
+ * Return: 0 (Success);
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+        
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
+return (0);
+}
