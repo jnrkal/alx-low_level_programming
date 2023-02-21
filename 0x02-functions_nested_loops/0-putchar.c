a52ef5d97f604b52a52039d3a36a22600d032c31
jnrkal <junioreric35@gmail>
2023-02-21 10:56:32 -0800

    0._putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * Main - A program that prints _putchar, followed by a new line.
- * Return: On success 1
- * On error, -1 is returned.
+ * _putchar - A program that prints _putchar, followed by a new line.
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
-
 {
 	return (write(1, &c, 1));
 }
