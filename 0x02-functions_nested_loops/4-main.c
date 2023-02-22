248248f64f3bef1efe160e21f134df8c49e72eb6
jnrkal <junioreric35@gmail>
2023-02-21 17:37:07 -0800

    isalpha

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -3,11 +3,16 @@
 /**
  *  _isalpha - checks for alphabetic character
  *  @c: the character for checking
- *  Return: 1 if c is a letter, lower or uppercase
+ *  Return: 1 for alphabetic character or 0 for anything else
  *  otherwise 0.
  */
 
 int _isalpha(int c)
 {
-	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
 }
